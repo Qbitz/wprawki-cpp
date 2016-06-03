0555e52e7c9eb374776e16270601fc692ced88c6
Damian Łoboda <konta.lobod@o2.pl>
2016-06-03 17:10:57 +0200

    projekt w trakcie

---
+++ zadanie2/program.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char** argv){
+	int h;
+	char c;
+	printf("Podaj wysokość trójkąta: \n");
+	scanf("%i",h);
+	char str[i*(2^i+1)];
+	printf("Podaj znak za pomocą którego narysowany będzie trójkat: \n");
+	scanf("%c",h);
+	printf("Generuje trójkąt!");
+	for (int i=0;i<=h:i++){
+		for (int j;j<=i;j++) strcpy(str,c);
+		strcpy ("\n"str,c);
+	}
+	printf("%c",str);
+	return 0;
+}
